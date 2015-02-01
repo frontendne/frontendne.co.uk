df8866f24d4802b07004a3c1a68b6565fef6ac89
sambeckham <sam@beckham.io>
2015-02-01 19:37:34 +0000

    Added random identities

--- dev/_assets/scripts/main.js
+++ dev/_assets/scripts/main.js
@@ -47,3 +47,51 @@ new validation(document.getElementById('mce-NAME'), {
     pattern: /[a-z]+/i
 });
 
+function identities(el, options) {
+    this.el = el;
+    this.options = extend( {}, this.options );
+    extend( this.options, options );
+    this._init();
+}
+
+identities.prototype.options = {
+    attr: 'placeholder',
+    data: []
+}
+
+identities.prototype._init = function() {
+    var self = this,
+        id = self.getRandomID();
+
+    self.populate(id);
+}
+
+identities.prototype.getRandomID = function() {
+    var self = this,
+        maxInt = self.options.data.length,
+        rand = Math.floor(Math.random() * maxInt),
+        id = self.options.data[rand];
+
+    return id;
+}
+
+identities.prototype.populate = function(id) {
+    var self = this,
+        attr = self.options.attr;
+
+    for( var key in id ) {
+        document
+            .getElementById(key)
+            .setAttribute(attr, id[key]);
+    }
+}
+
+new identities(document.getElementById('mailing-list__sign-up'), {
+    data: [{
+        'mce-NAME': 'Sam',
+        'mce-EMAIL': 'sam@beckham.io'
+    },{
+        'mce-NAME': 'Martin',
+        'mce-EMAIL': 'martin@tempertemper.net'
+    },]
+});
