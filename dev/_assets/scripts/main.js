0ff50c82653426c3ccedef84b166d1ec72b10be1
sambeckham <sam@beckham.io>
2015-02-22 17:26:40 +0000

    thrown together a map

--- dev/_assets/scripts/main.js
+++ dev/_assets/scripts/main.js
@@ -16,3 +16,8 @@ new randomIdentity(document.getElementById('mailing-list__sign-up'), {
 });
 
 new inlineSubmit(document.getElementById('mailing-list__sign-up'));
+
+
+require('mapbox.js');
+L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtZGJlY2toYW0iLCJhIjoiSVk5cS1UTSJ9.0lCowgljkS2VZ_8ToBkPUA';
+L.mapbox.map('map', 'samdbeckham.l9hmpn0e');
