2dac06fceb195068777028e760d7591b3a3256c4
sambeckham <sam@beckham.io>
2015-02-25 18:38:24 +0000

    Sorted the map out a little better

---
+++ dev/_assets/scripts/components/mapbox.js
@@ -0,0 +1,6 @@
+require('mapbox.js');
+
+L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtZGJlY2toYW0iLCJhIjoiSVk5cS1UTSJ9.0lCowgljkS2VZ_8ToBkPUA';
+L.mapbox.map('map', 'samdbeckham.l9hmpn0e',{
+    scrollWheelZoom: false
+});
--- dev/_assets/scripts/main.js
+++ dev/_assets/scripts/main.js
@@ -1,7 +1,8 @@
 var validation = require('./components/validation'),
     randomIdentity = require('./components/randomIdentity'),
     identities = require('./json/identities'),
-    inlineSubmit = require('./components/inlineSubmit');
+    inlineSubmit = require('./components/inlineSubmit'),
+    map = require('./components/mapbox');
 
 new validation(document.getElementById('mce-EMAIL'), {
     pattern: /\S+@\S+\.\S+/
@@ -16,8 +17,3 @@ new randomIdentity(document.getElementById('mailing-list__sign-up'), {
 });
 
 new inlineSubmit(document.getElementById('mailing-list__sign-up'));
-
-
-require('mapbox.js');
-L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtZGJlY2toYW0iLCJhIjoiSVk5cS1UTSJ9.0lCowgljkS2VZ_8ToBkPUA';
-L.mapbox.map('map', 'samdbeckham.l9hmpn0e');
