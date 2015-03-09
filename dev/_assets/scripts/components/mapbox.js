3beef8a7bc93fce3e3f4ca9d90c0b9a43ee09ebb
sambeckham <sam@beckham.io>
2015-03-09 19:35:42 +0000

    stopped the map dragging on touch

--- dev/_assets/scripts/components/mapbox.js
+++ dev/_assets/scripts/components/mapbox.js
@@ -1,6 +1,11 @@
 require('mapbox.js');
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoic2FtZGJlY2toYW0iLCJhIjoiSVk5cS1UTSJ9.0lCowgljkS2VZ_8ToBkPUA';
-L.mapbox.map('map', 'samdbeckham.l9hmpn0e',{
+var map = L.mapbox.map('map', 'samdbeckham.l9hmpn0e',{
     scrollWheelZoom: false
 });
+
+if (map.tap) {
+    map.dragging.disable();
+    map.tap.disable();
+}
