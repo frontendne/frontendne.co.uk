9517bc43185f82fd9da0b0b8c6a793d21af23867
Martin Underhill <martin@tempertemper.net>
2015-04-06 16:16:17 +0100

    abstracts page meta data to partials and analytics code to template

---
+++ web/cms/templates/layouts/events/head.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <title>Frontend NE – <?php
+      perch_collection('Events', [
+        'filter'   => 'slug',
+        'match'    => 'eq',
+        'value'    => perch_get('s'),
+        'count'    => 1,
+        'template' =>'/meta/event_title.html',
+      ]);
+    ?></title>
+    <meta name="description" content="<?php
+      perch_collection('Events', [
+        'filter'   => 'slug',
+        'match'    => 'eq',
+        'value'    => perch_get('s'),
+        'count'    => 1,
+        'template' =>'/meta/event_description.html',
+      ]);
+    ?>" />
+    <?php perch_layout('events/_open_graph'); ?>
+    <?php perch_layout('events/_twitter_card'); ?>
+    <?php perch_layout('global/_apple_touch_icon'); ?>
+    <?php perch_layout('global/_favicon'); ?>
+    <?php perch_content('Google Analytics'); ?>
+    <?php perch_get_css(); ?>
+</head>
+
+<?php
+  if (perch_layout_has('body-class')) {
+    echo '<body class="'.perch_layout_var('body-class', true).'">';
+  }else{
+    echo '<body>';
+  }
+?>
+
+<main role="main">
+
+    <?php perch_layout('homepage/header'); ?>
+
+    <div class="wrapper">
--- web/cms/templates/layouts/homepage/head.php
+++ web/cms/templates/layouts/homepage/head.php
@@ -3,22 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <title><?php perch_pages_title(); ?></title>
     <?php perch_page_attributes(); ?>
+    <?php perch_layout('global/_open_graph'); ?>
+    <?php perch_layout('global/_twitter_card'); ?>
     <?php perch_layout('global/_apple_touch_icon'); ?>
     <?php perch_layout('global/_favicon'); ?>
-
-    <title><?php perch_pages_title(); ?></title>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-59418357-1', 'auto');
-        ga('require', 'linkid', 'linkid.js');
-        ga('send', 'pageview');
-    </script>
+    <?php perch_content('Google Analytics'); ?>
     <?php perch_get_css(); ?>
 </head>
+
 <?php
   if (perch_layout_has('body-class')) {
     echo '<body class="'.perch_layout_var('body-class', true).'">';
