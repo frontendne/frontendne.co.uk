931ce668da3808a78016823b7a2e2e773c9eadc7
Martin Underhill <martin@tempertemper.net>
2015-04-06 15:30:47 +0100

    global head and header defined

---
+++ web/cms/templates/layouts/global/head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+    <meta name="description" content="A monthly meetup for Frontend developers in the North East of England, based in Newcastle upon Tyne." />
+    <?php perch_page_attributes(); ?>
+    <?php perch_layout('global/_apple_touch_icon'); ?>
+
+    <link rel="icon" href="/img/icons/favicon.png">
+    <!--[if IE]><link rel="shortcut icon" href="/img/icons/favicon.ico"><![endif]-->
+    <title><?php perch_pages_title(); ?></title>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-59418357-1', 'auto');
+        ga('require', 'linkid', 'linkid.js');
+        ga('send', 'pageview');
+    </script>
+    <?php perch_get_css(); ?>
+</head>
+<?php
+  if (perch_layout_has('body-class')) {
+    echo '<body class="'.perch_layout_var('body-class', true).'">';
+  }else{
+    echo '<body>';
+  }
+?>
+
+<main role="main">
+
+    <?php perch_layout('global/header'); ?>
+
+    <div class="wrapper">
---
+++ web/cms/templates/layouts/global/header.php
@@ -0,0 +1,8 @@
+<header class="main__header main__header--compact">
+    <div class="wrapper">
+        <a href="/" class="logo">
+            <h1 class="hide">Frontend NE</h1>
+            <?php include($_SERVER['DOCUMENT_ROOT'].'/cms/templates/includes/svg/logo.svg'); ?>
+        </a>
+    </div>
+</header>
