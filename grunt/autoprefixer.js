b33fba71935b0181cd6646fcf95bb7cd1ad90567
Martin Underhill <martin@tempertemper.net>
2015-04-06 14:18:16 +0100

    removes dev autoprefixer task

--- grunt/autoprefixer.js
+++ grunt/autoprefixer.js
@@ -2,14 +2,6 @@ module.exports = {
     options: {
         browsers: ['last 3 versions']
     },
-    dev: {
-        files: [{
-            expand: true,
-            cwd: '.tmp/css/',
-            src: '*.css',
-            dest: '.tmp/css/'
-        }]
-    },
     dist: {
         files: [{
             expand: true,
