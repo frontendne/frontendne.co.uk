f3a956fa65941189fe70b8017d30da1ef7bb2896
Martin Underhill <martin@tempertemper.net>
2015-04-06 15:29:43 +0100

    apple touch icon partial

---
+++ web/cms/templates/layouts/global/_apple_touch_icon.php
@@ -0,0 +1,9 @@
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-57x57.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-60x60.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-72x72.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-76x76.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-114x114.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-120x120.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-144x144.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-152x152.png') ?>" />
+<link rel="apple-touch-icon-precomposed" sizes="180x180" href="<?php perch_path('feathers/frontendne/img/icons/apple-touch-icon-precomposed-180x180.png') ?>" />
