e474fc3447b94e8efd6acf99ba3509e0c80eef4a
Martin Underhill <martin@tempertemper.net>
2015-04-06 14:08:27 +0100

    amends grunt copy for js, images, favicons and fonts

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,17 +1,28 @@
 module.exports = {
-    scripts: {
-        files: [{
-            expand: true,
-            dot: true,
-            cwd: 'dev/_assets/',
-            dest: '.tmp/',
-            src: 'scripts/{,*/}*.js'
-        }]
+    js: {
+      expand: true,
+      flatten: true,
+      src: [
+        'dev/_assets/components/html5shiv/dist/html5shiv.min.js'
+      ],
+      dest: 'web/cms/addons/feathers/frontendne/scripts/',
+    },
+    fonts: {
+        expand: true,
+        cwd: 'dev/_assets/fonts/',
+        src: '**',
+        dest: 'cms/addons/feathers/frontendne/fonts/',
+    },
+    favicon: {
+        expand: true,
+        cwd: 'dev/_assets/img/icons/',
+        src: 'favicon.ico',
+        dest: 'web/',
     },
     images: {
         expand: true,
         cwd: 'dev/_assets/img/',
         src: '**',
-        dest: 'web/img/',
+        dest: 'web/cms/addons/feathers/frontendne/img/',
     }
 };
