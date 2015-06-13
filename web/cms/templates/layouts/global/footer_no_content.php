a9a906e92524f9688d15d66f511aeaa0f22dea91
Colin Oakley <colin@htmlandbacon.com>
2015-06-13 22:17:58 +0100

    added 404 with blank footer

---
+++ web/cms/templates/layouts/global/footer_no_content.php
@@ -0,0 +1,6 @@
+
+    </main>
+    <?php perch_get_javascript(); ?>
+    <script src="https://js.tito.io/v1" async></script>
+</body>
+</html>
--- web/cms/templates/pages/errors/404.php
+++ web/cms/templates/pages/errors/404.php
@@ -1,3 +1,16 @@
+<?php
+
+  // Head and header
+  perch_layout('global/head', [
+    'body-class' => 'events',
+  ]);
+
+
+    ?>
 <h1>Sorry, that page could not be found.</h1>
 
 <?php perch_content('Error description'); ?>
+<?
+
+  // Main footer
+  perch_layout('global/footer_no_content');
